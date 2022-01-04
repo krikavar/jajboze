4e85fa57dacf5b0b8d02bbcfa5ab245137438367
Radek Křikava <krikavar.03@spst.cz>
2022-01-04 22:31:46 +0100

    Přidejte soubory projektu.

---
+++ jajboze/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace jajboze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button_click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 247);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(289, 97);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 246);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button_click
+            // 
+            this.button_click.Location = new System.Drawing.Point(503, 96);
+            this.button_click.Name = "button_click";
+            this.button_click.Size = new System.Drawing.Size(75, 23);
+            this.button_click.TabIndex = 2;
+            this.button_click.Text = "KLIKNI";
+            this.button_click.UseVisualStyleBackColor = true;
+            this.button_click.Click += new System.EventHandler(this.button_click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_click);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button_click;
+    }
+}
+
---
+++ jajboze/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jajboze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click_Click(object sender, EventArgs e)
+        {
+            try { 
+            char[] pole = textBox1.Text.ToCharArray();
+            int last = pole.Last();
+            int delka = pole.Length - 1;
+            char max = pole.Max();
+            if (last <= '9' && last >= '0'){
+                for(int i = 0; delka >= i; i++){
+                    if (pole[i] <= '9' && pole[i] >= '0'){
+                        pole[i] = 'x';
+                    }
+                }
+            }
+            else{
+                for (int i = 0; delka >= i; i++)
+                {
+                    if(delka == i){pole[i] = max;}
+                }
+            }
+            string vypis = new string(pole);
+            textBox2.Text = vypis;
+            }
+            catch { MessageBox.Show("PROBLEM"); }
+        }
+    }
+}
